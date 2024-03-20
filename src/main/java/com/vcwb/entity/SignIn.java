128618617700ed654401765fb446a7de238e7a8a
Sangram Nayak <sangramnay421@gmail.com>
2024-03-20 23:32:10 +0530

    code

---
+++ src/main/java/com/vcwb/entity/SignIn.java
@@ -0,0 +1,36 @@
+package com.vcwb.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@Entity
+@Getter
+@NoArgsConstructor
+@ToString
+@AllArgsConstructor
+@Table(name =  "SignIn", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+public class SignIn {
+	@Id
+	@GeneratedValue(strategy =  GenerationType.IDENTITY)
+	private Long id;
+	@Column
+	private String emailId;
+	@Column
+	private String password;
+
+	
+		
+		
+	}
+	
